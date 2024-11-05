95005683c25c33d757df6453fcaebe5151fa33d9
daianess1 <daiavrdossantos@gmail.com>
2024-11-05 11:46:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sobre mim',
+    'Qual minha idade?',
+    '18'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rondonia?',
+    'A capital é Porto Velho'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bom dia  em Inglês?',
+    'Oi em ingles é Good morning'
+)
